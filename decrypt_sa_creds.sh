be70757052c8c7462be8a7936ec7f7e5b240f50c
Akan Daniel <danielamahtoday@gmail.com>
2023-12-27 14:57:17 +0100

    Modified dot_yaml

--- decrypt_sa_creds.sh
+++ decrypt_sa_creds.sh
@@ -3,4 +3,6 @@
 mkdir $HOME/secrets
 gpg --quiet --batch --yes --decrypt --passphrase="$SA_CREDS_PASSPHRASE" \
 --output $HOME/secrets/sa-creds.json sa-creds.json.gpg
+echo "Decryption process finished."
+ls -l $HOME/secrets
 
