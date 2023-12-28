d933f7f810137caa52c26f1523b8d5fc89b0e714
Akan Daniel <danielamahtoday@gmail.com>
2023-12-28 18:51:18 +0100

    Handling secrets
    Handling secrets

--- main.py
+++ main.py
@@ -21,12 +21,11 @@ credentials = google.oauth2.credentials.Credentials.from_authorized_user_info(js
 st.code(code, language='python')
 
 credentials_json = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-credentials = google.oauth2.credentials.Credentials.from_authorized_user_info(json.loads(credentials_json))
+credentials = google.oauth2.service_account.Credentials.from_service_account_info(json.loads(credentials_json))
 
 endpoint = aiplatform.Endpoint(
     endpoint_name="projects/603505641991/locations/us-central1/endpoints/6876277550390181888"
 )
-
 
 # def generate_random_string(length):
 #     characters = string.ascii_letters + string.digits
