77a68254d111af36a671e1a7124ba2e0456804c7
Akan Daniel <danielamahtoday@gmail.com>
2023-12-28 18:03:40 +0100

    Handling secrets
    Handling secrets

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import string
 import json
 import random
 import streamlit as st
+import google.oauth2.credentials
 from google.cloud import aiplatform
 from google.cloud import secretmanager
 from google.oauth2 import service_account
@@ -10,68 +11,17 @@ from google.oauth2 import service_account
 
 print(os.environ['HOME'])
 st.write(os.environ['HOME'])
-# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '$HOME/secrets/sa-creds.json'
 
-# # Get the value of the environment variable
-# creds_json = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-#
-# # Parse the JSON string into a dictionary
-# creds_dict = json.loads(creds_json)
-#
-# # Create a Credentials object from the dictionary
-# creds = service_account.Credentials.from_service_account_info(creds_dict)
 
 code = """
 EMPTY
 """
 
 st.code(code, language='python')
-# from google.cloud import secretmanager
-# from google.oauth2 import service_account
-# import json
-#
-# def access_secret_version():
-#     # Create the Secret Manager client.
-#     client = secretmanager.SecretManagerServiceClient()
-#
-#     # Build the resource name of the secret version.
-#     name = "projects/603505641991/secrets/gac_sa-creds"
-#
-#     # Access the secret version.
-#     response = client.access_secret_version(request={"name": name})
-#
-#     # Return the decoded payload.
-#     return response.payload.data.decode('UTF-8')
-#
-# # Use the function to get the secret
-# creds_json = access_secret_version()
-#
-# # Parse the JSON string into a dictionary
-# creds_dict = json.loads(creds_json)
-#
-# # Create a Credentials object from the dictionary
-# creds = service_account.Credentials.from_service_account_info(creds_dict)
 
-# Replace 'path-to-your-service-account-file' with the path to your service account key file
-# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+credentials_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+credentials = google.oauth2.credentials.Credentials.from_authorized_user_file(credentials_path)
 
-
-# def access_secret_version():
-#     # Create the Secret Manager client.
-#     client = secretmanager.SecretManagerServiceClient()
-#
-#     # Build the resource name of the secret version.
-#     # name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-#     name = "projects/603505641991/secrets/gac_sa-creds/versions/latest"
-#
-#     # Access the secret version.
-#     response = client.access_secret_version(request={"name": name})
-#
-#     # Return the decoded payload.
-#     return response.payload.data.decode('UTF-8')
-#
-# # Use the function to get the secret
-# secret = access_secret_version()
 
 endpoint = aiplatform.Endpoint(
     endpoint_name="projects/603505641991/locations/us-central1/endpoints/6876277550390181888"
